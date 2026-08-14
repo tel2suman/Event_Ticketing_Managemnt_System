f7813ee31b5e315f6aa04bf0509b1d401373a126
suman <suman.sb30@gmail.com>
2026-08-14 13:33:04 +0530

    require config file for cloudinary

--- backend/app/config/cloudinary.js
+++ backend/app/config/cloudinary.js
@@ -1,5 +1,7 @@
 const cloudinary = require("cloudinary").v2;
 
+require("dotenv").config();
+
 // Configure the Cloudinary SDK using application environment credentials.
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
